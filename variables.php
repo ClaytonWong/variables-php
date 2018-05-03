0084e41209a73834f347ae461f30364ec0fd8117
ClaytonWong <c_w_serious-github@yahoo.com.au>
2018-05-03 18:08:55 +1000

    Add file.
    Add variables.php

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+  // Declare and initialise variables  my_str and MY_STR
+  $my_str = 'small';
+  $MY_STR = 'BIG';
+
+  // Display variables my_str and MY_STR to the browser
+  echo "Using echo:";
+  echo "<br>";
+  echo "my_str: $my_str";
+  echo "<br>";
+  echo "MY_STR: $MY_STR";
+  echo "<br>";
+  echo "<br>";
+  print "Using print:";
+  echo "<br>";
+  print "my_str: $my_str";
+  echo "<br>";
+  print "MY_STR: $MY_STR";
+?>
